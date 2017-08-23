c3337261b19f8f4b5fa510f9d337c51a95732ded
kazami <kazamizero@gmail.com>
2017-08-23 14:54:11 +0000

    test workaround

--- nnpack_ops_test.py
+++ nnpack_ops_test.py
@@ -44,36 +44,32 @@ class NNPackOpsTest(hu.HypothesisTestCase):
            pad=st.integers(0, 0),
            kernel=st.integers(5, 5),
            size=st.integers(32, 32),
-           input_channels=st.integers(1, 1),
-           output_channels=st.integers(1, 1),
-           batch_size=st.integers(1, 5),
+           input_channels=st.integers(1, 3),
+           output_channels=st.integers(8, 8),
+           batch_size=st.integers(1, 1),
            groups=st.integers(1, 1))
     def test_convolution_correctness(self, stride, pad, kernel, size,
                                      input_channels, output_channels,
                                      batch_size, groups):
         assume(input_channels % groups == 0)
         assume(output_channels % groups == 0)
-        assume(output_channels == input_channels / groups)
+        #assume(output_channels == input_channels / groups)
         assume(stride <= kernel)
         if stride != 1:
             assume(batch_size == 1)
 
         X = np.random.rand(
-            size, size, batch_size, input_channels)
-        X = X * 100
-        X = X.astype(np.uint8)
-        print(X)
+            batch_size, input_channels, size, size).astype(np.float32)
+        X = np.ceil(X)
         w = np.random.rand(
-            kernel, kernel, input_channels, output_channels)
-            
-        w = w * 10
-        w = w.astype(np.uint8)
-        b = np.random.rand(output_channels)
-        b = b * 10
-        b = b.astype(np.uint8)
+            output_channels, input_channels, kernel, kernel).astype(np.float32)
+        w = w * 2
+        w = np.ceil(w)
+        b = np.random.rand(output_channels).astype(np.float32)
+        b = np.ceil(b)
         order = "NCHW"
         outputs = {}
-        for engine in ["ARM"]:
+        for engine in ["", "ARM"]:
             op = core.CreateOperator(
                 "Conv",
                 ["X", "w", "b"],
@@ -86,13 +82,19 @@ class NNPackOpsTest(hu.HypothesisTestCase):
                 engine=engine,
                 group=groups,
             )
+            if engine == "ARM":
+                X = X.astype(np.uint8)
+                w = w.astype(np.uint8)
+                b = b.astype(np.uint8)
             self.ws.create_blob("X").feed(X)
             self.ws.create_blob("w").feed(w)
             self.ws.create_blob("b").feed(b)
             self.ws.run(op)
             outputs[engine] = self.ws.blobs["Y"].fetch()
+        print(outputs[""].shape)
+        print(outputs["ARM"].shape)
         np.testing.assert_allclose(
-            outputs["ARM"],
+            outputs[""],
             outputs["ARM"],
             atol=1e-4,
             rtol=1e-4)
